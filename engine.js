4a555fa64fd0af4c14817f1014b6880d75629a20
unknown <ash@ashbluewebdesign.com>
2011-10-21 01:47:45 -0500

    Massive upgrade to the engine with class extensions and entity spawning function. Will be ready for beta after everything has been properly consolidated from global variables to object variables.

--- engine.js
+++ engine.js
@@ -6,7 +6,7 @@ Author URL: http://twitter.com/#!/ashbluewd
 */
 
 /*----------
- Function library
+ Core library
 ----------*/
 // How to figure out what a user's computer can handle for frames with fallbacks
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
@@ -21,11 +21,94 @@ return  window.requestAnimationFrame       ||
         };
 })();
 
-// Loops through functions
+/* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/
+ * MIT Licensed.
+*/
+// Inspired by base2 and Prototype
+(function(){
+        var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+        // The base Class implementation (does nothing)
+        this.Class = function(){};
+        
+        // Create a new Class that inherits from this class
+        Class.extend = function(prop) {
+                var _super = this.prototype;
+                
+                // Instantiate a base class (but only create the instance,
+                // don't run the init constructor)
+                initializing = true;
+                var prototype = new this();
+                initializing = false;
+    
+                // Copy the properties over onto the new prototype
+                for (var name in prop) {
+                        // Check if we're overwriting an existing function
+                        prototype[name] = typeof prop[name] == "function" && 
+                        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+                        (function(name, fn){
+                                return function() {
+                                        var tmp = this._super;
+            
+                                        // Add a new ._super() method that is the same method
+                                        // but on the super-class
+                                        this._super = _super[name];
+            
+                                        // The method only need to be bound temporarily, so we
+                                        // remove it when we're done executing
+                                        var ret = fn.apply(this, arguments);        
+                                        this._super = tmp;
+                                        
+                                        return ret;
+                                };
+                        })(name, prop[name]) :
+                        prop[name];
+                }
+    
+                // The dummy class constructor
+                function Class() {
+                        // All construction is actually done in the init method
+                        if ( !initializing && this.init )
+                                this.init.apply(this, arguments);
+                }
+                
+                // Populate our constructed prototype object
+                Class.prototype = prototype;
+                
+                // Enforce the constructor to be what we expect
+                Class.prototype.constructor = Class;
+            
+                // And make this class extendable
+                Class.extend = arguments.callee;
+    
+                return Class;
+        };
+})();
+
+
+/*----------
+ Function library
+----------*/
+// Loops through objects
 function storageGet(property) {
         for (var i in storage) {
                 storage[i].draw();
         }
+}
+
+// Spawn an entity
+var id = 0;
+function spawnEntity(name,x,y) {
+        window['id' + id] = eval(new name);
+        window['id' + id].spawn(x,y);
+        id += 1;
+}
+
+// Generate a random number from min to max
+// Defaults to 1 for min
+function random(max,min) {
+        if (!min) min = 1;
+        return Math.floor(Math.random() * (max - min) + min);
 }
 
 
@@ -83,34 +166,30 @@ function draw() {
 /*-----------
  Entity Pallete
 -----------*/
-function Entity(values) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 0;
-        this.height = 0;
+var Entity = Class.extend({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
         
-        // Use array to construct values
-        if (!values) values = {};
-        for (var i in values) {
-                this[i] = values[i];
+        init: function() {
+                
+        },
+        update: function() {
+                
+        },
+        draw: function() {
+                
+        },
+        spawn: function(x,y) {
+                if (x) this.x = x;
+                if (y) this.y = y; 
+                this.init();
+                storage.push(this);
+        },
+        kill: function() {
+                for (var i in storage) {
+                        if(storage[i] == this) storage.splice(i,1);
+                }
         }
-}
-Entity.prototype.init = function() { // 1 time setup container
-
-};
-Entity.prototype.update = function() { // Controls entity logic
-        
-};
-Entity.prototype.draw = function() { // Output for literally drawing the item on Canvas
-        
-};
-Entity.prototype.spawn = function() { // Place new element into storage, must be manually called
-        this.init();
-        storage.push(this);
-};
-Entity.prototype.kill = function() { // Remove "this" from storage array        
-        for (var i in storage) {
-                if(storage[i] == this)
-                        storage.splice(i,1);
-        }
-};
+});
--- game.js
+++ game.js
@@ -2,21 +2,48 @@
  Running The Game 
 ------------*/
 // You can overwrite any of the core in start() and init here before firing onload
-window.onload = function() { var Game = new Core({ width:300, height:500 }); }
-
+window.onload = function() { var Game = new Core({ width:300, height:300 }); }
 
 /*------------
  Entity Objects
 ------------*/
 // example
-var Background = new Entity({x:20, y:100});
-Background.draw = function() {
-        ctx.fillStyle = '#000';
-        ctx.fillRect(this.x,this.y,canvas.width,canvas.height);
-};
+var Background = Entity.extend({
+        x: 0,
+        y: 0,
+        xAdd: 5,
+        yAdd: 5,
+        width: 20,
+        height: 20,
+        update: function() {
+                if (this.x + this.width > canvas.width) { this.x = canvas.width - this.width; this.xAdd = this.xAdd * -1; }
+                else if (this.x < 0) { this.x = 0; this.xAdd = this.xAdd * -1; }
+                if (this.y + this.height > canvas.height) { this.y = canvas.height - this.height; this.yAdd = random(5) * -1; }
+                else if (this.y < 0) { this.y = 0; this.yAdd = this.yAdd * -1; }
+                
+                this.x += this.xAdd;
+                this.y += this.yAdd;
+        },
+        draw: function() {
+                ctx.fillStyle = '#000';
+                ctx.fillRect(this.x,this.y,this.width,this.height);
+        }
+});
+
+//var Background = new Entity({x:20, y:100});
+//Background.draw = function() {
+//        ctx.fillStyle = '#000';
+//        ctx.fillRect(this.x,this.y,canvas.width,canvas.height);
+//};
 
 
 /*------------
  Entity Spawning
 ------------*/
-Background.spawn();
+spawnEntity(Background, 200, 50);
+spawnEntity(Background, 20, 20);
+
+//var id1 = new Background();
+//var id2 = new Background();
+//id1.spawn();
+//id2.spawn();
