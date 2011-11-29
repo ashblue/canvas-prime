b542f97e0879e081f2a3d71ef00c938fe6d98c73
Ashton Blue <ash@ashbluewebdesign.com>
2011-11-28 22:05:16 -0600

    Upgraded core engine into a single object with multiple hooks.

--- game.js
+++ game.js
@@ -1,38 +1,53 @@
-/*------------
- Running The Game 
-------------*/
-// You can overwrite any of the core in start() and init here before firing onload
-window.onload = function() { var Game = new Core({ width:300, height:300 }); }
+window.onload = function() {
+    /*------------
+    Running The Game 
+    -----------*/
+    // You can tweak your engine through the init hook and add your own custom utilities here
+    var MyEngine = Engine.extend({
+        width: 400,
+        height: 400
+    });
+      
+    // Create and activate your personal engine
+    var Game = new MyEngine();
+    Game.setup();
+        
+    // Animation must be kept seperate due to a DOM error caused by self-reference in your objects
+    function animate() {
+        requestAnimFrame( animate );
+        Game.draw();
+    }
+    animate();
+    
 
-/*------------
- Entity Objects
-------------*/
-// example
-var Background = Entity.extend({
-        x: 0,
-        y: 0,
+    /*------------
+     Entity Objects
+    ------------*/
+    // example
+    var Background = Entity.extend({
         xAdd: 5,
         yAdd: 5,
         width: 20,
         height: 20,
         update: function() {
-                if (this.x + this.width > canvas.width) { this.x = canvas.width - this.width; this.xAdd = this.xAdd * -1; }
-                else if (this.x < 0) { this.x = 0; this.xAdd = this.xAdd * -1; }
-                if (this.y + this.height > canvas.height) { this.y = canvas.height - this.height; this.yAdd = random(5) * -1; }
-                else if (this.y < 0) { this.y = 0; this.yAdd = this.yAdd * -1; }
-                
-                this.x += this.xAdd;
-                this.y += this.yAdd;
+            if (this.x + this.width > Game.width) { this.x = Game.width - this.width; this.xAdd = this.xAdd * -1; }
+            else if (this.x < 0) { this.x = 0; this.xAdd = this.xAdd * -1; }
+            if (this.y + this.height > Game.height) { this.y = Game.height - this.height; this.yAdd = Game.random(5) * -1; }
+            else if (this.y < 0) { this.y = 0; this.yAdd = this.yAdd * -1; }
+            
+            this.x += this.xAdd;
+            this.y += this.yAdd;
         },
         draw: function() {
-                ctx.fillStyle = '#000';
-                ctx.fillRect(this.x,this.y,this.width,this.height);
+            Game.ctx.fillStyle = '#000';
+            Game.ctx.fillRect(this.x,this.y,this.width,this.height);
         }
-});
-
-
-/*------------
- Entity Spawning
-------------*/
-spawnEntity(Background, 200, 50);
-spawnEntity(Background, 20, 20);
+    });
+        
+        
+    /*------------
+    Entity Spawning
+    ------------*/
+    Game.spawnEntity(Background, 200, 50);
+    Game.spawnEntity(Background, 20, 20);
+}
