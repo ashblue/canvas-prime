d933db48c361eefebc07c0c87dc4af2965812c47
Gary Koehl <gkoehl@gmail.com>
2012-05-08 19:27:05 -0500

    Added logic to the engine-output file to output a js file.

--- development/include/engine-output.php
+++ development/include/engine-output.php
@@ -5,8 +5,6 @@ Version: .01
 Desc: Assembles and strips unecessary content to create a straight text output for the engine.
 */
 
-header('Content-type: text/plain');
-
 // Retrive all functions
 include 'functions.php';
 
@@ -19,20 +17,20 @@ $file_num = count($files);
 foreach ($files as $index=>$file):
     // Get the contents
     $content = file_get_contents('../js/engine/' . $file, true);
-    
+
     // Strip the object creation declaration
     $content = str_replace('var cp = cp || {};', '', $content);
-    
+
     // Replace first instance of cp.
     $content = preg_replace('/cp./', '', $content, 1);
-    
+
     // Indent every freikin line with 4 spaces
     $content_format = '';
     $lines = preg_split("/(\r?\n)/", $content);
     foreach($lines as $ind=>$line){
         // Insert break with line
         $content_format .= $line;
-    
+
         // Exit on last item
         if ($ind + 1 === count($lines) && !($index + 1 === $file_num)):
             $content_format .= ',';
@@ -41,22 +39,25 @@ foreach ($files as $index=>$file):
 
 ';
         else:
-            $content_format .= '    
+            $content_format .= '
     ';
         endif;
     }
-    
+
     // Append content to body
     $body .= $content_format;
 endforeach;
-    
-// Echo the script
-echo 'header info here
 
-';
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Content-type: text/plain;\n");
+header("Content-Transfer-Encoding: binary");
+header("Content-Disposition: attachment; filename=\"engine-output.js\";\n\n");
+
+// Echo the script
+echo "header info here\n";
 
 //$body = file_get_contents('../js/engine/test1.js', true);
-echo 'cp = {';
-    echo $body;
-echo '};';
+echo "cp = {\n";
+    echo $body . "\n";
+echo "};\n";
 ?>
