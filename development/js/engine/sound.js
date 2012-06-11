db76453964dd722825f0fef2787cdd58a4a908d9
Ash Blue <ash@ashbluewebdesign.com>
2012-06-10 22:43:25 -0500

    Added most of the music player for soundtrack playback.

--- development/js/engine/sound.js
+++ development/js/engine/sound.js
@@ -11,10 +11,11 @@ there is no workaround for this other than prioritizing sounds.
 var cp = cp || {};
 
 cp.audio = {
+    url: 'audio/', // Url to access audio elements
+    storage: [], // Keeps track of all audio elements
     init: function() {
         this.detect();
     },
-    url: 'audio/', // Url to access audio elements
     
     // Searches for audio file support and sets it as necessary
     // Source: http://diveintohtml5.com/everything.html
@@ -31,6 +32,11 @@ cp.audio = {
         }
     },
     
+    // Toggles mute on or off
+    mute: function() {
+        
+    },
+    
     // Creates a new sound object
     Sound: Class.extend({
         init: function(name) {
@@ -43,19 +49,105 @@ cp.audio = {
         // pause
     }),
     
-    el: {
-        // Element storage location for audio element
-        store: '',
-        create: function() {
-            // http://stackoverflow.com/questions/10535124/javascript-create-audio-element-cross-browser-issue
+    // Handeling of game's music
+    music: {
+        random: false, // Randomly play a track when next is called
+        loop: false, // When the last track is finished playing, immediately start up the first track
+        repeat: false, // Replay the current track if it ends instead of advancing to the next
+        count: 0, // Currently played audio track index
+        
+        // playlist - an array of track names to load into the player
+        init: function(playlist) {
+            // Return an error if the playlist is not an array
+            if (typeof playlist !== array) {
+                console.error('Playlist passed to cp.audio.music was not an array and equal to ' + playlist);
+                return;
+            } else {
+                // Cache the playlist
+                this.playlist = playlist;
+            }
+            
+            // Load the first track
+            this.el = new Audio(cp.audio.url + this.playlist[0] + cp.audio.type);
+            
+            // Attach the event to play the next track
+        },
+        
+        // Return the name of the current track
+        trackName: function() {
+            var src = this.el.src;
+            
+            // Strip off the file type
+            src.replace(cp.audio.type, '');
+            
+            // Return just the name and nothing else of the url
+            var explode = src.split('/');
+            return explode.pop();
+        },
+        
+        // Fade effect
+        /*
+         begin (int) = 0 to 1 volume lv
+         end (int) = 0 to 1 volume lv
+         duration (int) = Time in seconds for the fade to span over ex. 1.25
+         stop (bool) = Whether or not to stop the track after the fade, defaults to false
+        */
+        fade: function(begin, end, duration, stop) {
+            
+        },
+        
+        /* Controls */
+        // Play currently location or pass an optional index for the playlist array
+        play: function(index) {
+            // No index, play as normal
+            if (index === undefined) {
+                this.el.play();
+
+            // Otherwise load and play the new index
+            } else {
+                this.count = index;
+                this.el.src = cp.audio.url + this.playlist[index] + cp.audio.type;
+            }
+        },
+        
+        stop: function() {
+            this.el.pause();
+            this.el.currentTime = 0;
+        },
+        
+        pause: function() {
+            this.el.pause();
+        },
+        
+        // Play next track
+        next: function() {
+            // Verify the next counter doesn't exceed the array length
+            
+            // If so bump it up
+            
+            // Load up the new src and play
+            this.play(this.count);
+        },
+        
+        // Play previous track
+        previous: function() {
+            // Verify the prev counter doesn't exceed the array length
+            
+            // If so bump it up
+            
+            // Load up the new src and play
+            this.play(this.count);
+        },
+        
+        // Set volume 0 to 1
+        volume: function(num) {
+            this.el.volume = num;
+        },
+        
+        // Add a new track to the array
+        // add (string) = Name of the file without the file type
+        add: function(name) {
+            this.playlist.push(name);
         }
-    },
-    
-    music: function() {
-        
-    },
-    
-    mute: function() {
-        
     }
 };
