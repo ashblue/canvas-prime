66656c1588c1708bf42d14497533f7ef38c7a42f
Ash Blue <ash@ashbluewebdesign.com>
2012-08-02 20:21:56 -0500

    Added docs for camera usage.

--- js/engine/camera.js
+++ js/engine/camera.js
@@ -1,20 +1,37 @@
-/*
-Name: Math processing
-Version: 1
-Desc: Contains a library of pre-built math equations, very
-useful for generating random numbers and simplifying other
-complex logic.
-*/
+/**
+ * Logic for controlling the game's camera.
+ * @author Ash Blue
+ */
 
 var cp = cp || {};
 
 (function (cp) {
     cp.camera = {
+        /** @type {number} X position of the camera form the top left */
         x: 10,
+
+        /** @type {number} Y position of the camera form the top left */
         y: 10,
+
+        /**
+         * Offset to clip visible items so you can better control what does
+         * and doesn't get drawn.
+         * @type {number} Number of pixels to offset the drawn items at each of
+         * the four corners. Positive numbers will yeild a larger drawing area,
+         * while a negative offset will shrink it.
+         * @todo Not currently integrated yet.
+         */
         visibleOffset: 50,
 
-        // Verifies the camera can see the item before drawing
+        //
+        /**
+         * Verifies the camera can see the item before drawing.
+         * @param {number} x Current x coordinate of the tested object.
+         * @param {number} y Current y coordinate of the tested object.
+         * @param {number} width Current width of the tested object.
+         * @param {number} height Current height of the tested object.
+         * @returns {boolean} False if it isn't visible, true if it is.
+         */
         visibleTest: function (x, y, width, height) {
             // Return false if outside boundaries
             if (x + width < this.x || // is object left of camera x
@@ -27,20 +44,40 @@ var cp = cp || {};
             return true;
         },
 
+        /**
+         * Sets the current position of the camera outside of the camera object.
+         * @param {number} x Position x of the camera from the top left.
+         * @param {number} y Position y of the camrae form the top left.
+         * @returns {self}
+         */
         setPosition: function (x, y) {
             this.x = x;
             this.y = y;
+
+            return this;
         },
 
-        // Sets and saves the translation through a standardized method, then clears Canvas drawing
+        /**
+         * Sets and saves the translation through a standardized method, then
+         * clears Canvas drawing area only in the current viewport.
+         * @returns {undefined}
+         */
         setViewport: function () {
             cp.ctx.save();
             cp.ctx.translate(-this.x, -this.y);
             cp.ctx.clearRect(this.x, this.y, cp.core.canvasWidth, cp.core.canvasHeight);
+
+            return;
         },
 
+        /**
+         * Restores the viewport to the previous state.
+         * @returns {undefined}
+         */
         restoreViewport: function () {
             cp.ctx.restore();
+            
+            return;
         }
     };
 }(cp));
